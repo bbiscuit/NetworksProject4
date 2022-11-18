bdc7abc11ac451e804c8980105b2e77691a75bf4
Andrew Huffman <55068248+bbiscuit@users.noreply.github.com>
2022-11-18 11:59:39 -0500

    Update pa4_transmission_generator.py

--- pa4_transmission_generator.py
+++ pa4_transmission_generator.py
@@ -38,26 +38,59 @@ ENCODING_MAP = {"A" : [1,1,1,1,1], \
                 "?" : [-1,-1,-1,-1,1], \
                 "!" : [-1,-1,-1,-1,-1]}
 
+def dotProduct(v1, v2) -> int:
+	"""Finds the dot-product of the two given vectors."""
+
+	if not len(v1) == len(v2):
+		raise Exception(f'Vector lengths were not the same. len(v1) = {len(v1)}, len(v2) = {len(v2)}')
+
+	outp = 0
+	for i in range(0, len(v1)):
+		outp += v1[i] * v2[i]
+
+	return outp
+
 def encodeStringToCDMA(stringInput, cdmaCode) :
-    stringEncodedCDMA = []
-    for letter in stringInput :
-        letterEncoding = ENCODING_MAP[letter]
-        letterEncodedSignal = []
-        for letterBit in letterEncoding :
-            letterEncodedCDMASignal = []
-            for cdmaBit in cdmaCode :
-                letterEncodedCDMASignal.append(letterBit*cdmaBit)
+    stringEncodedCDMA = [] # The result list.
+    for letter in stringInput : # For each letter in the given string...
+        letterEncoding = ENCODING_MAP[letter] # Encode to a list of values
+        letterEncodedSignal = [] 
+        for letterBit in letterEncoding : # For each value in the encoding, scale by every code in the chipping sequence
+            letterEncodedCDMASignal = [] 
+            for cdmaBit in cdmaCode : # For every bit in what I think is the chipping sequence
+                letterEncodedCDMASignal.append(letterBit*cdmaBit) # Encode the encoding value by multiplying by the chipping sequence.
             letterEncodedSignal.append(letterEncodedCDMASignal)
-        stringEncodedCDMA.append(letterEncodedSignal)
+        stringEncodedCDMA.append(letterEncodedSignal) # Append the finally encoded signal.
     return stringEncodedCDMA
+    
+def decodeTransmission(combinedTras, code):
+    """Gets the transmission in the combined transmission with the given code. Assumes that the combined transmission has already been
+    split into sections the same length as the code."""
+    
+    # ALGORITHM:
+    # 1. Dot-product each section by the code.
+    # 2. Interpret positive dot-products as 1s, and negative dot-products as 0s, and return the
+    # transmission.
+    
+    result = []
+    
+    # 1. Dot-product each by the code.
+    
+    for x in combinedTrans:
+        result.append(dotProduct(x, code))
+    
+    print(result)
+    
+    # 2. Interpret positive dot-products as 1s, and negative dot-products as 0s, and return the
+    # transmission.
 
 def addTransmissions(t1, t2) :
     # this simple method only works for same length strings
-    if(len(t1)!=len(t2)) : return 0
-    sumOfTransmissions = []
-    for letter in range(len(t1)) :
-        letterEncodedSignal = []
-        for letterBit in range(len(t1[letter])) :
+    if(len(t1)!=len(t2)) : return 0 # Guard clause
+    sumOfTransmissions = [] # The output list: the two transmission values added together.
+    for letter in range(len(t1)) : # for every index in transmission 1...
+        letterEncodedSignal = [] 
+        for letterBit in range(len(t1[letter])) : # For every bit in the asci-encoded character...
             letterEncodedCDMASignal = []
             for cdmaBit in range(len(t1[letter][letterBit])) :
                 letterEncodedCDMASignal.append(t1[letter][letterBit][cdmaBit] + t2[letter][letterBit][cdmaBit])
@@ -79,6 +112,8 @@ trans1 = encodeStringToCDMA(M1,C1)
 trans2 = encodeStringToCDMA(M2,C2)
 
 combinedTrans = addTransmissions(trans1, trans2)
+
+decodeTransmission(combinedTrans, C1)
       
 outfile = open('transmission.txt', 'w')
 outputRawTransmission(combinedTrans, outfile)
